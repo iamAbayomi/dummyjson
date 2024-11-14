21848adb851a37d9839a8e94679ca75b11878462
iamAbayomi <oladinifolawiyo@gmail.com>
2024-11-14 21:16:09 +0000

    fix: build error with static pages

--- src/app/search/[productId]/page.tsx
+++ src/app/search/[productId]/page.tsx
@@ -1,29 +1,45 @@
 // app/search/[productId]/page.tsx
+"use client";
 
-import { fetchProductById, fetchProducts } from '../../../services/api';
-import ProductDetail from '../../../components/ProductDetail';
-import { Product } from '../../../types/product';
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { Product } from "../../../types/product";
+import Image from "next/image";
+import { fetchProductById } from "@/services/api";
 
-interface ProductPageProps {
-  params: { productId: string };
-}
+const ProductPage: React.FC = () => {
+  const { productId } = useParams(); // Access productId from route params
+  const [product, setProduct] = useState<Product | null>(null);
 
-const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
-  const product: Product = await fetchProductById(parseInt(params.productId));
+  useEffect(() => {
+    const fetchData = async () => {
+      if (productId) {
+        const fetchedProduct = await fetchProductById(
+          parseInt(productId.toString()) // Convert to number if necessary
+        );
+        setProduct(fetchedProduct);
+      }
+    };
+    fetchData();
+  }, [productId]);
+
+  if (!product) return <p>Loading...</p>;
 
   return (
-    <div>
-      <ProductDetail product={product} />
+    <div className="product-detail">
+      <Image
+        className="mx-auto"
+        width={500}
+        height={500}
+        src={product.thumbnail}
+        alt={product.title}
+      />
+      <h1 className="mb-[10px] font-[600] text-[16px]">{product.title}</h1>
+      <p className="text-[14px]">{product.description}</p>
+      <p className="mt-[20px] text-[12px]">Price: ${product.price}</p>
+      <button className="mt-[10px]">Add to Cart</button>
     </div>
   );
 };
 
 export default ProductPage;
-
-export async function generateStaticParams() {
-  // Pre-generate pages for a set of product IDs
-  const products = await fetchProducts('');
-  return products.slice(0, 10).map((product) => ({
-    productId: product.id.toString(),
-  }));
-}
